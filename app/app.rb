7978966be1cf6fe4562fb5b37b8467ed5dc07b3f
kris <krispeshi@gmx.com>
2024-03-25 12:21:31 +0200

    first commit

---
+++ app/app.rb
@@ -0,0 +1,87 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+#noinspection RubyResolve
+require 'sqlite3'
+
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/images' do
+  erb :index
+end
+
+#get '/layout1' do
+#erb :layout1
+#end
+
+
+#get '/sent' do
+#erb :sent
+#end
+
+require 'sinatra'
+#noinspection RubyResolve
+require 'sqlite3'
+
+# Method connect with database
+#noinspection RubyResolve
+def get_db
+  SQLite3::Database.new 'base.db'
+end
+
+# Configure application
+configure do
+  db = get_db
+  db.execute 'CREATE TABLE IF NOT EXISTS "Messages"
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "username" TEXT,
+      "phone" TEXT,
+      "email" TEXT,
+      "option" TEXT,
+      "comment" TEXT
+    )'
+  db.close
+end
+
+# Method save form data to database
+def save_form_data_to_database
+  db = get_db
+  db.execute 'INSERT INTO Messages (username, phone, email, option, comment)
+  VALUES (?, ?, ?, ?, ?)', [@username, @phone, @email, @option, @comment]
+  db.close
+end
+
+# Index page with form
+get '/index1' do
+  @title = 'Форма за заявки на Sinatra (Ruby)'
+  erb :index1
+end
+
+post '/' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @email = params[:email]
+  @option = params[:option]
+  @comment = params[:comment]
+
+  save_form_data_to_database
+
+  @title = 'Благодаря, вашето сообщение е изпратено'
+  erb :sent
+end
